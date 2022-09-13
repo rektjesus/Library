df923993543f27e19971e97043c15986ba427fd4
sleepless <arnas.manucan@gmail.com>
2022-09-13 16:40:20 +0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,23 @@
+let myLibrary = [];
+
+class Book {
+    constructor() {
+        this.author = author;
+        this.title = title;
+        this.pages = pages;
+        this.read = read;
+    }
+};
+
+function displayBook() {
+    // display book on page
+};
+
+function addBookToLibrary(array, book) {
+
+}
+
+function updateDisplay() {
+
+};
+
