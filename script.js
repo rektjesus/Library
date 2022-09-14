28fda6bc7ebca3df34a3fe5f1c4605f015827d49
sleepless <arnas.manucan@gmail.com>
2022-09-14 14:26:14 +0300

    Create function to add books to Library array

--- script.js
+++ script.js
@@ -1,23 +1,57 @@
-let myLibrary = [];
-
-class Book {
-    constructor() {
-        this.author = author;
-        this.title = title;
-        this.pages = pages;
-        this.read = read;
+let myLibrary = [
+    {
+        title: "A Game of Thrones",
+        author: "George R. R. Martin",
+        pages: 695,
+        read: false
     }
+];
+
+let titleInp = document.getElementById('title');
+let authorInp = document.getElementById('author');
+let pagesInp = document.getElementById('pages');
+let readInp = document.getElementById('check');
+const btnAdd = document.querySelector('#add_book');
+
+function Book(title, author, pages, read) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
 };
 
 function displayBook() {
-    // display book on page
+
 };
-
-function addBookToLibrary(array, book) {
-
-}
 
 function updateDisplay() {
 
 };
+
+function addBookToLibrary(myLibrary) {
+    let title = titleInp.value;
+    let author = authorInp.value;
+    let pages = pagesInp.value;
+    let read = checkRead(readInp);
+
+    let newBook = new Book(title, author, pages, read);
+
+    myLibrary.push(newBook);
+};
+
+function checkRead(readInp) {
+    let read = readInp.value;
+    if (read === 'on') {
+        return true;
+    }
+    else {
+        return false;
+    }
+};
+
+btnAdd.addEventListener('click', () => {
+    addBookToLibrary(myLibrary);
+});
+
+
 
